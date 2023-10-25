13311a388dc4e97206c43e0692c1e473476b48e3
Marcos EngC <socramborges991@gmail.com>
2023-10-25 16:57:24 -0400

    adiciondo usuario

--- target/classes/com/biblioteca/Banco.sql
+++ target/classes/com/biblioteca/Banco.sql
@@ -8,8 +8,10 @@ CREATE TABLE livros (
     disp VARCHAR(1) NOT NULL,
     cnpj_editora INT NOT NULL,
     id_autor INT NOT NULL,
+    cpf_usuario INT NOT NULL,
     FOREIGN KEY (id_autor) REFERENCES autor(id),
-    FOREIGN KEY (cnpj_editora) REFERENCES editora(cnpj)
+    FOREIGN KEY (cnpj_editora) REFERENCES editora(cnpj),
+    FOREIGN KEY (cpf_usuario) REFERENCES usurario(cpf),
 
 );
  
@@ -21,5 +23,27 @@ CREATE TABLE editora (
 CREATE TABLE autor (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(30) NOT NULL
+
+);
+
+CREATE TABLE ususario(
+    cpf INT PRIMARY KEY,
+    nome VARCHAR(30) NOT NULL,
+    senha INT NOT NULL,
+    email VARCHAR(50),
+    cnpj_editora INT NOT NULL,
+    id_autor INT NOT NULL,
+    FOREIGN KEY (id_autor) REFERENCES autor(id),
+    FOREIGN KEY (cnpj_editora) REFERENCES editora(cnpj)
+
+);
+CREATE TABLE emprestimo(
+    id INT PRIMARY KEY,
+    dataEmp DATE NOT NULL,
+    dataDevolucao DATE NOT NULL,
+    isbn_livro INT NOT NULL,
+    cpf_usuario INT NOT NULL,
+    FOREIGN KEY (isbn_livro) REFERENCES livros(isbn),
+    FOREIGN KEY (cpf_usuario) REFERENCES usuario(cpf)
 
 );
